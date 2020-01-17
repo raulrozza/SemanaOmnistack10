c1a24b896bd2b04a149d40bc5dfaca7776079f35
Raul Rosá <raul.hmetal@gmail.com>
2020-01-17 13:27:10 -0300

    Second class

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { port, mongoUrl } = require('./config');
 const routes = require('./routes');
 
@@ -10,6 +11,7 @@ mongoose.connect(mongoUrl, {
     useUnifiedTopology: true,
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
